4fa37ec87773dff35fe8c4701da48febb9875256
Imac <imac@iMac-de-Imac.local>
2022-01-08 09:32:08 -0500

    [ADD] angular material implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('src/app/home/home.module').then(m => m.HomeModule),
-  },
-  {
-    path: 'courses',
-    loadChildren: () => import('src/app/courses/courses.module').then(m => m.CoursesModule),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('src/app/home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'courses',
+        loadChildren: () => import('src/app/courses/courses.module').then(m => m.CoursesModule),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ]
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { MaterialUiModule } from './material-ui/material-ui.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    BrowserAnimationsModule,
+    MaterialUiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
