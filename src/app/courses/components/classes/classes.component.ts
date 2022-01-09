5d52f8d5271564425bace1ce24d097b5879e3c27
Fabian David Dueñas <fabian0896@outlook.com>
2022-01-09 15:26:54 -0500

    [UPDATE] keep working on details page

--- src/app/courses/components/classes/classes.component.ts
+++ src/app/courses/components/classes/classes.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from 'src/app/models/course';
+
+import { faBook, faClock, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-classes',
+  selector: 'app-classes[classes]',
   templateUrl: './classes.component.html',
   styleUrls: ['./classes.component.scss']
 })
 export class ClassesComponent implements OnInit {
+  @Input() classes!: Content[]
+
+  selected = 0;
+
+  bookIcon = faBook;
+  clockIcon = faClock;
+  playIcon = faPlay;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeSelected(index: number) {
+    this.selected = index;
   }
 
 }
